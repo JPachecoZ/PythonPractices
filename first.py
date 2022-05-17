1f5c94edf3ba0e7e632016a65e3710ed393d6a3b
jpachecoz <jepach@gmail.com>
2022-05-17 13:23:01 -0500

    construct a map function with python

--- first.py
+++ first.py
@@ -1,65 +1,15 @@
-candidates = []
+string_array = ["1", "2", "3", "4"]
 
-def get_list(prompt, min, max):
-  candidates_list = input(prompt).split()
-  while len(candidates_list)<min or len(candidates_list) > max:
-    print("The list should have between", min, "and", max, "elements")
-    candidates_list = input(prompt).split()
-  
-  return candidates_list
+def string_to_square(string):
+  return int(string) ** 2
 
-def get_positive(prompt):
-  voters = input(prompt)
-  while voters.isdigit() == False or int(voters) <= 0:
-    print("Number should be positive")
-    voters = input(prompt)
+def my_map(function, data):
+  newData = []
+  for element in data:
+    new_element = function(element)
+    newData.append(new_element)
+  return newData
 
-  return int(voters)
-    
+print(list(map(lambda x: int(x)**2, string_array)))
 
-
-def valid_vote(name):
-  for element in candidates:
-    if element["name"] == name:
-      element["votes"] += 1
-      return True
-  
-  return False
-
-def print_winner():
-  winners = []
-  winner = 0
-  for element in candidates:
-    if element["votes"] > winner:
-      winner = element["votes"]
-      winners = [element["name"]]
-    elif element["votes"] == winner:
-      winners.append(element["name"])
-  print("Winners: ", ", ".join(winners)) if len(winners)> 1 else print("Winners: ", winners[0])
-  
-
-
-
-
-
-names = get_list("Candidates names: ", 1, 9)
-
-for i in names:
-  candidate = { "name" : i, "votes" : 0 }
-  candidates.append(candidate)
-
-voters_count = get_positive("Number of voters: ")
-print(voters_count)
-
-print("-" * 20)
-
-for i in range(1, voters_count + 1):
-  prompt = "Voter " + str(i) + ": "
-  name = input(prompt)
-  while valid_vote(name) == False:  
-    print("Invalid vote")
-    name = input(prompt)
-
-print("-" * 20)
-
-print_winner()
+print(my_map(lambda x: int(x)**2, string_array))
